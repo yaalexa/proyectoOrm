07dce514621a911b56dfb2de1ffe4b24fbce426d
yaalexa25 <yaalexa25@gmail.com>
2025-02-27 18:23:29 -0500

    cambio

--- router/usuarioRouter.js
+++ router/usuarioRouter.js
@@ -3,3 +3,4 @@ const router = express.Router();
 const UsuarioController = require("../controllers/usuarioController");
 router.get("/usuario", UsuarioController.listarUsuarios);
 module.exports = router;
+//creado por: alexandra
